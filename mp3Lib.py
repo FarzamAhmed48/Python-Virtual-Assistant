3ff643bdcba550d6c5aa6a76b4a45064e3bb957f
Farzam Ahmed <farzamahmed48@gmail.com>
2025-03-01 23:00:04 +0500

    Mp3 lib added

---
+++ mp3Lib.py
@@ -0,0 +1,5 @@
+music ={
+    "wolf":"https://www.youtube.com/watch?v=S3clhok7TRY",
+    "lion":"https://www.youtube.com/watch?v=itGqAFrVrEs",
+    "tiger":"https://www.youtube.com/watch?v=T94PHkuydcw"
+}
